60f44bea1eae488fa6a3145285d52d660cf936af
Rogerio Fialho <rogerio.fialho@totvs.com.br>
2018-12-26 15:50:38 -0200

    Ajuste Sample Calendar

--- sample-thf-calendar-basic/src/app/app.component.ts
+++ sample-thf-calendar-basic/src/app/app.component.ts
@@ -1,30 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
-import { ThfCheckboxGroupOption } from '@totvs/thf-ui/components/thf-field';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'thf-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit { 
-  src: string;
-  size: string;
-
-  sizeOptions: Array<ThfCheckboxGroupOption> = [
-    { label: 'Extra Large (144x144)', value: 'xl' },
-    { label: 'Large (96x96)', value: 'lg' },
-    { label: 'Medium (64x64)', value: 'md' },
-    { label: 'Small (32x32)', value: 'sm' },
-    { label: 'Extra small (24x24)', value: 'xs' }
-  ];
-
-  ngOnInit() {
-    this.restore();
-  }
-
-  restore() {
-    this.src = 'http://lorempixel.com/144/144/cats';
-    this.size = undefined;
-  }
-
-}
+export class AppComponent { }
